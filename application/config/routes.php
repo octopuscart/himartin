a8596effe08e8ad02fb1046bcf29e2a54e5a674f
dev1 <dev@octopuscart.com>
2019-04-22 11:08:15 +0530

    cahgnes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-@\=';
+
 $route['default_controller'] = 'Shop';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
