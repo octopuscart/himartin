e926b6983794f8f8bd084d142760494cb0ecfc87
dev <dev@develop.com>
2019-05-25 15:25:00 +0530

    changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -62,7 +62,6 @@ $route['faqs'] = 'Shop/faqs';
 $route['lookbook/(:any)'] = 'Shop/lookbook_style/$1';
 $route['Products/CustomSuits'] = 'Product/ProductList/2/0';
 $route['stylingTips'] = 'Shop/stylingTips';
-
 $route['stylingTipsTag'] = 'Shop/stylingTipsTag';
 $route['styleTips/(:any)/(:any)'] = 'Shop/styleTipsDetails/$1/$2';
 
