6e8ca00dc2a514b04fb6591655c43f9b7fe34b68
dev <dev@develop.com>
2019-05-25 15:41:01 +0530

    changes

--- application/config/configdbconnect.php
+++ application/config/configdbconnect.php
@@ -1,9 +1,9 @@
 <?php
 $globleConnectDB = array();
 try {
-    $username = "j2k5e6r5_octopus";
+    $username = "a937118_hongkong";
     $password = "India$2017";
-    $conn = new PDO('mysql:host=localhost;dbname=j2k5e6r5_hongkong', $username, $password);
+    $conn = new PDO('mysql:host=localhost;dbname=a937118_hongkongbt', $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
      $stmt = $conn->prepare('SELECT * FROM configuration_site');
     $stmt->execute();
