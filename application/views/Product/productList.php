e9650ca94b204ed0c307212182d0a00d6669275d
dev <dev@develop.com>
2019-07-04 02:41:21 +0530

    changes

--- application/views/Product/productList.php
+++ application/views/Product/productList.php
@@ -27,7 +27,42 @@ $image2 = "";
                 echo $custom_item;
                 ?>
             </ul>
+            <?php
+            switch ($custom_id) {
+                case "1":
+                    ?>
+                               <h4>To view a much wider collection for custom made shirts and feel fabrics, book a fitting now</h4>
+
+                    <?php
+                    break;
+                case "2":
+                    ?>
+            <h4>To view a much wider collection for custom made suits and feel fabrics, book a fitting now.</h4>
+                    <?php
+                    break;
+                case "5":
+                    ?>
             <h4>To Get Real Feel Of Fabrics By Touch - Book A Fitting</h4>
+                    <?php
+                    break;
+                case "3":
+                    ?>
+            <h4>To Get Real Feel Of Fabrics By Touch - Book A Fitting</h4>
+                    <?php
+                    break;
+                case "4":
+                    ?>
+            <h4>To view a much wider collection for custom made suits and feel fabrics, book a fitting now.</h4>
+                    <?php
+                    break;
+                default:
+                    ?>
+            <h4>To view a much wider collection for custom made suits and feel fabrics, book a fitting now.</h4>
+            <?php
+            
+            }
+            ?>
+
 
         </div>
     </section>
@@ -37,82 +72,82 @@ $image2 = "";
     <div class="shop-page-area" >
         <div class="container">
             <div class="row"  ng-if="productResults.products.length">
-               <!--                <aside class="col-lg-2 col-md-2 col-sm-2 m_bottom_70 m_xs_bottom_30" style="    padding: 0;">
-                    categories
-                    <?php
-                    if (count($categories)) {
+                <!--                <aside class="col-lg-2 col-md-2 col-sm-2 m_bottom_70 m_xs_bottom_30" style="    padding: 0;">
+                     categories
+<?php
+if (count($categories)) {
+    ?>
+                             <div class="m_bottom_40 m_xs_bottom_30">
+                                 <h5 class="color_dark fw_light m_bottom_10 m_top_10">Categories</h5>
+                                 <ul class="categories_list">
+    <?php
+    foreach ($categories as $key => $value) {
+        $subcategories = $value['sub_category'];
+        ?> 
+         
+                                                                                         <a href="<?php echo site_url("Product/ProductList/" . $custom_id . "/" . $value['id']); ?>" class="color_dark tr_all d_block">
+                                                                 <span class="icon_wrap_size_0 circle d_inline_m m_right_8 color_grey_light_5 tr_inherit">
+                                                                     <i class="icon-angle-right"></i>
+                                                                 </span>
+        <?php echo $value['category_name']; ?>
+                                                             </a>
+         
+                                             <a href="#" class="color_dark tr_all d_block">
+                                                 <span class="icon_wrap_size_0 circle d_inline_m m_right_8 color_grey_light_5 tr_inherit">
+                                                     <i class="icon-angle-right"></i>
+                                                 </span>
+        <?php echo $value['category_name']; ?>
+                                             </a>
+                        <?php
+                        if (count($subcategories)) {
+                            ?>
+                                                     <ul class="fw_light d_none">
+                            <?php
+                            foreach ($subcategories as $key1 => $value1) {
+                                ?>
+                                                                 <li>
+                                                                     <a href="<?php echo site_url("Product/ProductList/" . $custom_id . "/" . $value1['id']); ?>" class="d_block color_dark"><i class="icon-angle-right d_inline_m m_right_8 color_grey_light_5 fs_small tr_inherit"></i><span class="d_inline_m"><?php echo $value1['category_name']; ?></span></a>
+                                                                 </li>
+                <?php
+            }
+            ?>
+                                                     </ul>
+                            <?php
+                        }
                         ?>
-                        <div class="m_bottom_40 m_xs_bottom_30">
-                            <h5 class="color_dark fw_light m_bottom_10 m_top_10">Categories</h5>
-                            <ul class="categories_list">
-                                <?php
-                                foreach ($categories as $key => $value) {
-                                    $subcategories = $value['sub_category'];
-                                    ?> 
-
-                                                                                <a href="<?php echo site_url("Product/ProductList/" . $custom_id . "/" . $value['id']); ?>" class="color_dark tr_all d_block">
-                                                        <span class="icon_wrap_size_0 circle d_inline_m m_right_8 color_grey_light_5 tr_inherit">
-                                                            <i class="icon-angle-right"></i>
-                                                        </span>
-                                    <?php echo $value['category_name']; ?>
-                                                    </a>
-
-                                    <a href="#" class="color_dark tr_all d_block">
-                                        <span class="icon_wrap_size_0 circle d_inline_m m_right_8 color_grey_light_5 tr_inherit">
-                                            <i class="icon-angle-right"></i>
-                                        </span>
-                                        <?php echo $value['category_name']; ?>
-                                    </a>
-                                    <?php
-                                    if (count($subcategories)) {
-                                        ?>
-                                        <ul class="fw_light d_none">
-                                            <?php
-                                            foreach ($subcategories as $key1 => $value1) {
-                                                ?>
-                                                <li>
-                                                    <a href="<?php echo site_url("Product/ProductList/" . $custom_id . "/" . $value1['id']); ?>" class="d_block color_dark"><i class="icon-angle-right d_inline_m m_right_8 color_grey_light_5 fs_small tr_inherit"></i><span class="d_inline_m"><?php echo $value1['category_name']; ?></span></a>
-                                                </li>
-                                                <?php
-                                            }
-                                            ?>
-                                        </ul>
-                                        <?php
-                                    }
-                                    ?>
-                                    <?php
-                                }
-                                ?>  
-                            </ul>
-                        </div>
-
-
-
                         <?php
                     }
-                    ?>
-
-
-
-
-
-                    <div class="product_attr" ng-repeat="(attrk, attrv) in productResults.attributes" ng-if="attrv.length > 1">
-                        <div class="m_bottom_45 m_xs_bottom_30">
-                            manufacturers
-                            <div class="m_bottom_25">
-                                <h5 class="color_dark fw_light m_bottom_10 m_bottom_10">{{attrk}}</h5>
-                                <ul>
-                                    <li class="m_bottom_10"  ng-repeat="atv in attrv">
-                                        <input type="checkbox" checked id="checkbox_{{atv.id}}" name="" class="d_none" ng-model="atv.checked" ng-click="attributeProductGet(atv)">
-                                        <label for="checkbox_{{atv.id}}" class="d_inline_m m_right_10  color_dark">{{atv.attribute_value}}  <span class="color_grey">({{atv.product_count}})</span></label>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </aside>-->
+                    ?>  
+                                 </ul>
+                             </div>
+     
+     
+     
+    <?php
+}
+?>
+ 
+ 
+ 
+ 
+ 
+                     <div class="product_attr" ng-repeat="(attrk, attrv) in productResults.attributes" ng-if="attrv.length > 1">
+                         <div class="m_bottom_45 m_xs_bottom_30">
+                             manufacturers
+                             <div class="m_bottom_25">
+                                 <h5 class="color_dark fw_light m_bottom_10 m_bottom_10">{{attrk}}</h5>
+                                 <ul>
+                                     <li class="m_bottom_10"  ng-repeat="atv in attrv">
+                                         <input type="checkbox" checked id="checkbox_{{atv.id}}" name="" class="d_none" ng-model="atv.checked" ng-click="attributeProductGet(atv)">
+                                         <label for="checkbox_{{atv.id}}" class="d_inline_m m_right_10  color_dark">{{atv.attribute_value}}  <span class="color_grey">({{atv.product_count}})</span></label>
+                                     </li>
+                                 </ul>
+                             </div>
+                         </div>
+                     </div>
+ 
+ 
+                 </aside>-->
 
 
 
@@ -126,7 +161,7 @@ $image2 = "";
                                     <!--images container-->
                                     <div class="fp_images relative">
                                         <img class="img-responsive" src="<?php echo custome_image_server; ?>/{{product.folder}}.jpg" alt="product" style="background: white;">
-                                                                           <img class="img-responsive" src="<?php echo custome_image_server; ?>/{{product.folder}}.jpg" alt="product" style="background: white;">
+                                        <img class="img-responsive" src="<?php echo custome_image_server; ?>/{{product.folder}}.jpg" alt="product" style="background: white;">
 
                                     </div>
                                 </div>
@@ -141,11 +176,11 @@ $image2 = "";
 
                                     <div class="clearfix fp_buttons">
                                         <div class="half_column w_md_full animate_fctl tr_all f_left with_ie f_md_none m_md_bottom_10">
-                                            <a href="<?php echo site_url("booking");?>"  class="button_type_6 d_inline_b color_pink transparent r_corners vc_child tr_all add_to_cart_button"><span class="d_inline_m clerarfix"><i class="icon-pencil f_left m_right_10 fs_large"></i><span class="fs_medium">Book A Fitting</span></span></a>
+                                            <a href="<?php echo site_url("booking"); ?>"  class="button_type_6 d_inline_b color_pink transparent r_corners vc_child tr_all add_to_cart_button"><span class="d_inline_m clerarfix"><i class="icon-pencil f_left m_right_10 fs_large"></i><span class="fs_medium">Book A Fitting</span></span></a>
                                         </div>
-<!--                                        <div class="half_column w_md_full animate_fctr tr_all f_left clearfix with_ie f_md_none">
-                                            <a href="#" class="button_type_6 relative tooltip_container f_right f_md_none d_md_inline_b d_block color_dark r_corners vc_child tr_all color_blue_hover t_align_c"><i class="icon-basket d_inline_m fs_large"></i><span class="d_block r_corners color_default tooltip fs_small fw_normal tr_all">Add to Cart</span></a>
-                                        </div>-->
+                                        <!--                                        <div class="half_column w_md_full animate_fctr tr_all f_left clearfix with_ie f_md_none">
+                                                                                    <a href="#" class="button_type_6 relative tooltip_container f_right f_md_none d_md_inline_b d_block color_dark r_corners vc_child tr_all color_blue_hover t_align_c"><i class="icon-basket d_inline_m fs_large"></i><span class="d_block r_corners color_default tooltip fs_small fw_normal tr_all">Add to Cart</span></a>
+                                                                                </div>-->
                                     </div>
                                 </figcaption>
                             </figure>
@@ -174,7 +209,7 @@ $image2 = "";
             </div>
 
         </div>
-        
+
     </div>
     <!-- Shop Page Area End Here -->
 
